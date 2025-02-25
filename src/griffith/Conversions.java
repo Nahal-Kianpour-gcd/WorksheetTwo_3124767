ae69e34d0b5cd8466bedc5cbb7671918115d02a3
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-02-25 22:23:50 +0000

    Added Conversions.java with method stubs

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -2,4 +2,31 @@ package griffith;
 
 public class Conversions {
 
+	// Converts Euro to Dollar
+	public double euroToDollar(double euro) {
+		// Implementation will be here later
+
+	}
+
+	// Converts Dollar to Euro
+	public double dollarToEuro(double dollar) {
+		// Implementation will be here later
+
+	}
+
+	// Converts String to Integer
+	public int stringToInteger(String val) {
+		// Implementation will be here later
+
+	}
+
+	// Converts Integer to String
+	public String integerToString(int val) {
+
+	}
+
+	// Switchs uppercase letters to lowercase and vice versa
+	public String switchCase() {
+
+	}
 }
