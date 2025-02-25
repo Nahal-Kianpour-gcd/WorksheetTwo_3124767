08816c61a69616deb1eccd79c5c75407c9d33b9b
Nahal Kianpour <nahal.kianpourlirharani@student.griffith.ie>
2025-02-25 22:33:14 +0000

    Added dummy return statements in Conversions.java methods for initial test failures

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -5,28 +5,28 @@ public class Conversions {
 	// Converts Euro to Dollar
 	public double euroToDollar(double euro) {
 		// Implementation will be here later
-
+		return 0.0;
 	}
 
 	// Converts Dollar to Euro
 	public double dollarToEuro(double dollar) {
 		// Implementation will be here later
-
+		return 0.0;
 	}
 
 	// Converts String to Integer
 	public int stringToInteger(String val) {
 		// Implementation will be here later
-
+		return 0;
 	}
 
 	// Converts Integer to String
 	public String integerToString(int val) {
-
+		return null;
 	}
 
 	// Switchs uppercase letters to lowercase and vice versa
 	public String switchCase() {
-
+		return null;
 	}
 }
